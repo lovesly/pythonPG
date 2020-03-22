7ff2a409e413f7c40e07adb94d1b8d435c098576
zzh <zzhlovesly@outlook.com>
2020-03-22 23:46:16 +0800

    loops

---
+++ bilibili/PyForEverybody/p23.py
@@ -0,0 +1,11 @@
+# string part1
+fruit = 'banana'
+for letter in fruit:
+  print(letter)
+
+# equivalent
+index = 0
+while index < len(fruit):
+  letter = fruit[index]
+  print(letter)
+  index = index + 1
