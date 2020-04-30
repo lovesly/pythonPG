312b3d4fe5157d98281bc3e83fd8de5ff7097f81
Zihan Zhang <zzhlovesly@outlook.com>
2020-05-01 00:12:51 +0800

    ssl and bs4

--- bilibili/PyForEverybody/chp12/p51.py
+++ bilibili/PyForEverybody/chp12/p51.py
@@ -1,13 +1,19 @@
 # python2 doesn't have request method
+# and it seems like we've installed bs4 into python2 env
+# to use it in python3, we have to install it again??
 # import urllib.request, urllib.parse, urllib.error
 import urllib
 from bs4 import BeautifulSoup
+import ssl
+
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
 
 # have to type in 'http://www.dr-chunk.com/page1.htm'
-# so fucking weirdc
 url = input('Enter - ')
 # read??
-html = urllib.urlopen(url).read()
+html = urllib.urlopen(url, context=ctx).read()
 soup = BeautifulSoup(html, 'html.parser')
 
 # Retrieve all of the anchor tags
